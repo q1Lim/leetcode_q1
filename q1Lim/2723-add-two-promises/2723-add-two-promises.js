946a014098c4505d8e4a28485bfe280ea20ef9d3
q1Lim <cochlea00@gmail.com>
2026-01-22 17:26:06 +0900

    Time: 54 ms (58.29%), Space: 54.7 MB (32.97%) - LeetHub

---
+++ q1Lim/2723-add-two-promises/2723-add-two-promises.js
@@ -0,0 +1,17 @@
+/**
+ * @param {Promise} promise1
+ * @param {Promise} promise2
+ * @return {Promise}
+ */
+var addTwoPromises = async function(promise1, promise2) {
+    const data1 = await promise1;
+    const data2 = await promise2;
+
+    return data1 + data2;
+    
+};
+
+/**
+ * addTwoPromises(Promise.resolve(2), Promise.resolve(2))
+ *   .then(console.log); // 4
+ */
